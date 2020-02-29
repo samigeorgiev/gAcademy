73fa62c8e6ceb1203be115c4e0c11580426cc9b7
Samuil Georgiev <sami.georgiev@gmail.com>
2020-02-29 20:05:51 +0200

    [Fix] replaced depricated load w/ proto-loader

--- microservices/auth/index.js
+++ microservices/auth/index.js
@@ -1,12 +1,16 @@
 const path = require('path');
 
 const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
 
 const authenticationServices = require('./services/authentication');
 
-const proto = grpc.load(path.join(__dirname, process.env.PROTO_PATH));
+const protoDefinition = protoLoader.loadSync(path.join(__dirname, process.env.PROTO_PATH));
+const protoDescriptor = grpc.loadPackageDefinition(protoDefinition);
+
 const server = new grpc.Server();
 
-console.log(authenticationServices);
+server.addService(protoDescriptor.auth.Authentication.service, authenticationServices);
 
-server.addService(proto.auth.Authentication.service, authenticationServices);
+server.bind('0.0.0.0:8000', grpc.ServerCredentials.createInsecure());
+server.start();
