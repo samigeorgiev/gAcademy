baf52daea17e329a3edc89c05e449862c873c37b
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-01 20:34:29 +0200

    [Update] added login

--- microservices/auth/services/authenticationService.js
+++ microservices/auth/services/authenticationService.js
@@ -1,13 +1,28 @@
+const bcrypt = require('bcrypt');
 const grpc = require('grpc');
 const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
 const User = require('../models/user');
 
-exports.signUp = async (call, callback) => {    
-    const { email, password, firstName, lastName, accountType, gender } = call.request;
+const PASSWORD_LENGTH = { min: 8, max: 64 };
+const SALT_ROUNDS = 10;
 
-    if (!email || !password || !firstName || !lastName || accountType === 'NOT_SET') {
+exports.signUp = async (call, callback) => {    
+    let { email, password, firstName, lastName, accountType, gender } = call.request;
+
+    if (
+        (!email || !password || !firstName || !lastName || accountType === 'NOT_SET') ||
+        (!validator.isEmail(email) || !validator.isLength(password, PASSWORD_LENGTH))
+    ) {
         return callback(grpc.status.INVALID_ARGUMENT, null);
+    }
+
+    email = validator.normalizeEmail(email);
+    try {
+        password = await bcrypt.hash(password, SALT_ROUNDS);
+    } catch (error) {
+        return callback(grpc.status.INTERNAL, null);
     }
 
     try {
@@ -24,7 +39,7 @@ exports.signUp = async (call, callback) => {
     } catch (error) {
         return callback(grpc.status.INTERNAL, null);
     }
-    
+
     const token = jwt.sign(
         { userId: user.id },
         process.env.JWT_SECRET,
@@ -33,6 +48,36 @@ exports.signUp = async (call, callback) => {
     callback(null, { token, expiresIn: process.env.JWT_VALID_TIME, userId: user.id });
 };
 
-exports.logIn = (call, callback) => {
+exports.logIn = async (call, callback) => {
+    let { email, password } = call.request;
+    if (!email || !password || !validator.isEmail(email)) {
+        return callback(grpc.status.INVALID_ARGUMENT, null);
+    }
+    email = validator.normalizeEmail(email);
 
+    let user;
+    try {
+        user = await User.findOne({ where: { email }});
+    } catch (error) {
+        return callback(grpc.status.INTERNAL);
+    }
+
+    if (!user) {
+        return callback(grpc.status.NOT_FOUND, null);
+    }
+
+    try {
+        if (!(await bcrypt.compare(password, user.password))) {
+            return callback(grpc.status.UNAUTHENTICATED, null);
+        }
+    } catch (error) {
+        return callback(grpc.status.INTERNAL, null);
+    }
+
+    const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_VALID_TIME }
+    );
+    callback(null, { token, expiresIn: process.env.JWT_VALID_TIME, userId: user.id });
 };
