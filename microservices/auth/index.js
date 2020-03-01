a774d3cc5b5f43fa9538e81987dd10160f77c208
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-01 17:22:37 +0200

    [Fix] Added support for enum in proto + fixed typo

--- microservices/auth/index.js
+++ microservices/auth/index.js
@@ -8,7 +8,11 @@ const authenticationService = require('./services/authenticationService');
 const authorizationService = require('./services/authorizationService');
 
 const protoDefinition =
-    protoLoader.loadSync(path.join(__dirname, process.env.PROTO_PATH));
+    protoLoader.loadSync(path.join(__dirname, process.env.PROTO_PATH), {
+        keepCase: true,
+        enums: String,
+        defaults: true
+    });
 const protoDescriptor = grpc.loadPackageDefinition(protoDefinition);
 
 const server = new grpc.Server();
@@ -16,7 +20,7 @@ const server = new grpc.Server();
 server.addService(protoDescriptor.auth.Authentication.service, authenticationService);
 server.addService(protoDescriptor.auth.Authorization.service, authorizationService);
 
-const port = process.env.PORT ?? 8000;
+const port = process.env.PORT || 8000;
 if (process.env.NODE_ENV == 'production') {
 } else {
     server.bind(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure());
