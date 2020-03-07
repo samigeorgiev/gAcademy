106650433976a3e261c6a6578b5a6c7cc6970089
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-07 17:45:51 +0200

    [Update] Added layout + toolbar comp

---
+++ client/src/components/navigation/Toolbar.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Logo from '../UI/Logo.svelte';
+    import SearchBar from './SearchBar.svelte';
+
+    // export let segment;
+
+    let searchValue;
+    const searchSubmit = event => console.log(event);
+</script>
+
+<nav>
+    <div class="logo-container"><Logo /></div>
+    <div class="search-bar-container">
+        <SearchBar bind:value={searchValue} on:submit={searchSubmit} />
+    </div>
+</nav>
+
+<style>
+    nav {
+        height: 3rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-bottom: 2px solid #aaa;
+    }
+
+    .logo-container {
+        height: 60%;
+        margin-right: 1rem;
+    }
+
+    .search-bar-container {
+        height: 75%;
+    }
+</style>
--- client/src/routes/_layout.svelte
+++ client/src/routes/_layout.svelte
@@ -1,6 +1,12 @@
 <script>
-	// export let segment;
+	import Toolbar from '../components/navigation/Toolbar.svelte';
+
+	export let segment;
 </script>
+
+<header>
+	<Toolbar {segment} />
+</header>
 
 <main>
 	<slot></slot>
