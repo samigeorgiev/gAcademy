2b1380cd90c7b249e686c145a4f2b65c072c7981
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-07 18:42:56 +0200

    [Update] added comp to list all categories

---
+++ client/src/components/navigation/Categories.svelte
@@ -0,0 +1,69 @@
+<script>
+    export let categories;
+</script>
+
+<div class="categories">
+    <button>
+        <img src="images/categories.svg" alt="categories">
+    </button>
+    <div class="tooltip">
+        <ul>
+            {#each categories as category}
+                <li>{category}</li>
+            {/each}
+        </ul>
+    </div>
+</div>
+
+<style>
+    .categories {
+        position: relative;
+        height: 80%;
+        margin-right: .5rem;
+    }
+
+    button {
+        height: 100%;
+        padding: .5rem;
+        margin: auto;
+        background: transparent;
+        border: none;
+        border-radius: 4px;
+    }
+
+    button:hover {
+        cursor: pointer;
+        background: #eee;
+    }
+
+    button:focus {
+        outline: none;
+    }
+
+    button img {
+        height: 100%;
+    }
+
+    .tooltip {
+        position: absolute;
+        height: 20rem;
+        width: 15rem;
+        background: #ccc;
+    }
+
+    .tooltip::after {
+        content: " ";
+        position: absolute;
+        bottom: 100%;
+        left: 50%;
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent transparent #ccc transparent;
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+</style>
--- client/src/components/navigation/Toolbar.svelte
+++ client/src/components/navigation/Toolbar.svelte
@@ -1,17 +1,25 @@
 <script>
+    import Categories from './Categories.svelte';
     import Logo from '../UI/Logo.svelte';
     import SearchBar from './SearchBar.svelte';
 
     // export let segment;
+
+    const categories = ["Math", "IT"];
 
     let searchValue;
     const searchSubmit = event => console.log(event);
 </script>
 
 <nav>
-    <div class="logo-container"><Logo /></div>
-    <div class="search-bar-container">
-        <SearchBar bind:value={searchValue} on:submit={searchSubmit} />
+    <div class="logo-container">
+        <a href="/"><Logo /></a>
+    </div>
+    <div class="navigation-container">
+        <Categories {categories} />
+        <div class="search-bar-container">
+            <SearchBar bind:value={searchValue} on:submit={searchSubmit} />
+        </div>
     </div>
 </nav>
 
@@ -20,13 +28,19 @@
         height: 3rem;
         display: flex;
         align-items: center;
-        justify-content: center;
+        justify-content: space-between;
         border-bottom: 2px solid #aaa;
     }
 
     .logo-container {
         height: 60%;
-        margin-right: 1rem;
+        margin-left: 1rem;
+    }
+
+    .navigation-container {
+        display: flex;
+        align-items: center;
+        height: 100%;
     }
 
     .search-bar-container {
