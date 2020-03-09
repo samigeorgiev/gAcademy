b98e94d80b5f18681d0c4f031b9cae69152f98dd
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-09 18:17:38 +0200

    [Update] finished Course component

--- client/src/components/navigation/navigationItems/Courses.svelte
+++ client/src/components/navigation/navigationItems/Courses.svelte
@@ -1,8 +1,41 @@
 <script>
     import NavItem from './NavItem.svelte';
+
+    const courses = ['1st', '2nd'];
 </script>
 
-<div class="courses">
-    <!-- <button>My Courses</button>
-    <Dropdown>ss</Dropdown> -->
+<div class="nav-item-container">
+    <NavItem>
+        <div class="content">
+            <span>My courses</span>
+        </div>
+        <ul slot="dropdown">
+            {#each courses as course}
+                <li>{course}</li>
+            {/each}
+        </ul>
+    </NavItem>
 </div>
+
+<style>
+    .nav-item-container {
+        /* margin-right: 1rem;  */
+        padding-right: .5rem;
+        border-right: 1px solid #555;
+    }
+
+    .content {
+        display: flex;
+        align-items: flex-end;
+        color: #555;
+    }
+
+    .content::after {
+        content: "";
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent;
+        margin-left: .5rem;
+    }
+
+</style>
