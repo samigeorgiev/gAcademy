361aa0ce04f154f63e15dfa8abe1e776822b6ca4
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-10 08:55:10 +0200

    [Update] Added drawer toggle for side drawer

---
+++ client/src/components/navigation/DrawerToggle.svelte
@@ -0,0 +1,25 @@
+<div class="drawer-toggle" on:click>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+
+<style>
+    .drawer-toggle {
+        display: flex;
+        flex-flow: column;
+        justify-content: space-around;
+        align-items: center;
+        width: 6rem;
+        height: 100%;
+        padding: 1rem;
+        box-sizing: border-box;
+        cursor: pointer;
+    }
+
+    .drawer-toggle div {
+        width: 90%;
+        height: 3px;
+        background-color: white;
+    }
+</style>
