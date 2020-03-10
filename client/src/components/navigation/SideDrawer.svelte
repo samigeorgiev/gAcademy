da02bd9b550d3eac16dc8e054693a00b0d31e630
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-10 10:00:24 +0200

    [Update] SideDrawer almost finished

--- client/src/components/navigation/SideDrawer.svelte
+++ client/src/components/navigation/SideDrawer.svelte
@@ -1,11 +1,23 @@
-<div class="side-drawer">
+<script>
+    import {fly} from 'svelte/transition';
 
+    import Backdrop from '../UI/Backdrop.svelte';
+
+    export let close;
+</script>
+
+<Backdrop on:click={close} />
+<div class="side-drawer" transition:fly={{x: -200}}>
+    <slot></slot>
 </div>
 
 <style>
     .side-drawer {
         position: fixed;
+        top: 0;
+        left: 0;
         height: 100%;
-        width: 10rem;
+        width: 20rem;
+        background: white;
     }
 </style>
