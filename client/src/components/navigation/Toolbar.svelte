d3ae1d9a02fa1438db7d3b106dddd750a0f6f55d
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-10 09:42:55 +0200

    [Update] Added media q to toolbar

--- client/src/components/navigation/Toolbar.svelte
+++ client/src/components/navigation/Toolbar.svelte
@@ -3,11 +3,12 @@
     import Calendar from './navigationItems/Calendar.svelte';
     import Categories from './navigationItems/Categories.svelte';
     import Courses from './navigationItems/Courses.svelte';
+    import DrawerToggle from './DrawerToggle.svelte';
     import Logo from '../UI/Logo.svelte';
     import SearchBar from './navigationItems/SearchBar.svelte';
 </script>
 
-<nav>
+<nav class="nav-desktop">
     <div class="logo-container">
         <a href="/"><Logo /></a>
     </div>
@@ -24,14 +25,24 @@
     </div>
 </nav>
 
+<nav class="nav-mobile">
+    <DrawerToggle />
+    <div class="logo-container"><Logo /></div>
+    <div class="search-bar-container">
+        <SearchBar />
+    </div>
+</nav>
+
 <style>
     nav {
         padding: .5rem;
         height: 3rem;
-        max-width: 80rem;
-        margin: 0 auto;
         display: flex;
         justify-content: space-between;
+    }
+
+    .nav-desktop {
+        display: none;
     }
 
     .logo-container {
@@ -40,19 +51,34 @@
         margin-left: 1rem;
     }
 
-    .navigation-container {
-        display: flex;
-        align-items: center;
-        height: 100%;
-    }
-
     .search-bar-container {
-        height: 75%;
+        height: 100%;
     }
 
-    .account-container {
-        display: flex;
-        align-items: center;
-        height: 100%;
+    @media (min-width: 60rem) {
+        .nav-mobile {
+            display: none;
+        }
+
+        .nav-desktop {
+            max-width: 80rem;
+            margin: 0 auto;
+        }
+
+        .navigation-container {
+            display: flex;
+            align-items: center;
+            height: 100%;
+        }
+
+        .search-bar-container {
+            height: 75%;
+        }
+
+        .account-container {
+            display: flex;
+            align-items: center;
+            height: 100%;
+        }
     }
 </style>
