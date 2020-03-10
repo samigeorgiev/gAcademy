4ecd70fb2edbf67e881d1c5a1e828e0867b41217
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-10 11:53:28 +0200

    [Fix] added class directive + resp design

--- client/src/components/UI/Dropdown.svelte
+++ client/src/components/UI/Dropdown.svelte
@@ -1,47 +1,53 @@
 <script>
     export let isShown;
     export let position;
-
-    let display;
-    $: display = isShown ? 'block' : 'none';
 </script>
 
 <div 
-    class="dropdown {position}"
+    class="dropdown"
+    class:shown={isShown}
+    class:right={position === 'right'}
     on:mouseenter
     on:mouseleave
-    style="display: {display}"
 >
     <slot></slot>
 </div>
 
 <style>
     .dropdown {
-        position: absolute;
-        top: 2.75rem;
-        background: white;
-        box-shadow: -.25px .25px 10px .25px rgba(0, 0, 0, .75);
-        border-radius: 2px;
+        display: none;
     }
 
-    .dropdown.right {
-        right: 0;
-    }
+    @media (min-width: 60rem) {
+        .dropdown {
+            position: absolute;
+            top: 2.75rem;
+            background: white;
+            box-shadow: -.25px .25px 10px .25px rgba(0, 0, 0, .75);
+            border-radius: 2px;
+        }
 
-    .dropdown::after {
-        content: "";
-        position: absolute;
-        bottom: 100%;
-        border-width: .5rem;
-        border-style: solid;
-        border-color: transparent transparent white transparent;
-    }
+        .dropdown.right {
+            right: 0;
+        }
 
-    .dropdown.left::after {
-        left: .75rem;
-    }
+        .shown {
+            display: block;
+        }
 
-    .dropdown.right::after {
-        right: .75rem;
+        .dropdown::after {
+            content: "";
+            position: absolute;
+            bottom: 100%;
+            left: .75rem;
+            border-width: .5rem;
+            border-style: solid;
+            border-color: transparent transparent white transparent;
+        }
+
+        .dropdown.right::after {
+            left: auto;
+            right: .75rem;
+        }
     }
 </style>
