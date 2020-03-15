e80d909239014ee670082667d670c5f1dd65ffda
Samuil Georgiev <sami.georgiev@gmail.com>
2020-03-15 07:21:20 +0200

    [Update] Added auth button

---
+++ client/src/components/navigation/navigationItems/AuthButton.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let color;
+</script>
+
+<button on:click class:red={color === 'red'}>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        width: 5rem;
+        padding: .75rem;
+        background: none;
+        border: 1px solid #555;
+        color: #555;
+        cursor: pointer;
+        font: inherit;
+        font-weight: 600;
+    }
+
+    button:focus {
+        outline: none;
+    }
+
+    button:first-of-type {
+        margin-right: .5rem;
+    }
+
+    .red {
+        color: white;
+        background: red;
+    }
+</style>
