cd4a81d3988beb494df8eb7f7be036754205bfc8
Samuil Georgiev <sami.georgiev@gmail.com>
2020-04-21 13:18:25 +0300

    cd

--- microservices/authentication/it/run-db-on-docker.sh
+++ microservices/authentication/it/run-db-on-docker.sh
@@ -1,4 +1,4 @@
-docker run --name authentication_it_db -e POSTGRES_PASSWORD=postgres -e PGPASSWORD=postgres -p 5432:5432 --rm -d postgres
+docker run --name authentication_it_db -e POSTGRES_PASSWORD=postgres -e PGPASSWORD=postgres --network=host --rm -d postgres
 docker cp ./prepare-db.sql authentication_it_db:/prepare-db.sql
 docker exec authentication_it_db sleep 1
 docker exec authentication_it_db psql -U postgres -f prepare-db.sql
