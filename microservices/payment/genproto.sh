7a7ebe560c277f1f3a6f8202422e5164c3365fc8
Samuil Georgiev <sami.georgiev@gmail.com>
2020-04-27 15:40:58 +0300

    [Update] Added dockerfile + test

--- microservices/payment/genproto.sh
+++ microservices/payment/genproto.sh
@@ -1,4 +1,11 @@
+if [ ! -d "env" ]
+then
+    python3.8 -m venv env
+    ./env/bin/pip install grpcio-tools==1.28.1
+fi
+
 mkdir -p proto
+
 ./env/bin/python -m grpc_tools.protoc -I../../proto --python_out=./proto --grpc_python_out=./proto ../../proto/*
 
 cd proto
