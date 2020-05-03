a75e6836fc02c11f2057035653660163dd50f499
toniyanchev <anton.t.y2002@gmail.com>
2020-05-03 14:49:29 +0300

    [UPDATE] added get video to resource management

--- microservices/resource-management/index.js
+++ microservices/resource-management/index.js
@@ -3,7 +3,6 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const _ = require('lodash');
 
 const app = express();
 
@@ -20,29 +19,29 @@ const port = process.env.PORT || 8004;
 
 app.use(express.static('resources'));
 
-app.listen(port, () => 
-  console.log(`Port ${port}.`)
+app.listen(port, () =>
+    console.log(`Port ${port}.`)
 );
 
 app.post('/upload', async (req, res) => {
     try {
-        if(!req.files) {
+        if (!req.files) {
             res.send({
                 status: false,
                 message: 'No file uploaded'
             });
         } else {
-            let avatar = req.files.avatar;
-            
-            avatar.mv('./resources/' + 'video.mp4');
+            const video = req.files.video;
+
+            video.mv('./resources/' + 'video.mp4');
 
             res.send({
                 status: true,
                 message: 'File is uploaded',
                 data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
+                    name: video.name,
+                    mimetype: video.mimetype,
+                    size: video.size
                 }
             });
         }
@@ -51,4 +50,7 @@ app.post('/upload', async (req, res) => {
     }
 });
 
-
+app.get('/download', function(req, res) {
+    const video = `${__dirname}/resources/video.mp4`;
+    res.download(video);
+});
