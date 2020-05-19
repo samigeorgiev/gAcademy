7ba3ec2d6c527c0a0678fba6abb643eaaf65c540
Samuil Georgiev <sami.georgiev@gmail.com>
2020-05-19 13:08:25 +0300

    df

--- microservices/payment/genproto.sh
+++ microservices/payment/genproto.sh
@@ -1,26 +1,4 @@
-PYTHON=./env/bin/python
-
-if [ "$1" = "container" ]
-then
-    PYTHON=$(which python)
-fi
-
-if [ ! -d "env" -a "$1" != "container" ]
-then
-    python3.8 -m venv env
-    ./env/bin/pip install grpcio-tools==1.28.1
-fi
+#!/bin/bash
 
 mkdir -p proto
-
-$PYTHON -m grpc_tools.protoc -I../../proto --python_out=./proto --grpc_python_out=./proto ../../proto/*
-
-cd proto
-
-for FILE in $(ls .)
-do
-    if [[ $FILE == *grpc.py ]]
-    then
-        sed -i '4s/^/from . /' $FILE
-    fi
-done
+cp ../../proto/*.proto ./proto
