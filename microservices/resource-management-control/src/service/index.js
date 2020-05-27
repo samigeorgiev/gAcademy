2f69e8a82bd0ddc924981ce37ac37f127059179e
toniyanchev <anton.t.y2002@gmail.com>
2020-05-27 19:32:43 +0300

    [FIX] errors

--- microservices/resource-management-control/src/service/index.js
+++ microservices/resource-management-control/src/service/index.js
@@ -1,13 +1,25 @@
 const service = require('./service');
+const grpc = require('grpc');
+const errorHandler = require('../errorHandler.js');
 
 exports.createLecture = async (call, callback) => {
     service.createLecture(call, callback);
 };
 
 exports.getLecture = async (call, callback) => {
+    const { id } = call.request;
+    if (!id || id == null) {
+        const status = grpc.status.INVALID_ARGUMENT;
+        return errorHandler(callback, status, 'Blank request');
+    }
     service.getLecture(call, callback);
 };
 
 exports.getAllLectures = async (call, callback) => {
+    const { courseId } = call.request;
+    if (!courseId || courseId == null) {
+        const status = grpc.status.INVALID_ARGUMENT;
+        return errorHandler(callback, status, 'Blank request');
+    }
     service.getAllLectures(call, callback);
 };
--- microservices/resource-management-control/src/service/service.js
+++ microservices/resource-management-control/src/service/service.js
@@ -1,9 +1,9 @@
-// const grpc = require('grpc');
+const grpc = require('grpc');
 
 const { getRepository } = require('typeorm');
 const { getConnection } = require('typeorm');
 
-// const errorHandler = require('../util/errorHandler');
+const errorHandler = require('../errorHandler');
 const Lecture = require('../entity/LectureSchema.js');
 const Resource = require('../entity/ResourceSchema.js');
 
@@ -35,23 +35,51 @@ exports.createLecture = async (call, callback) => {
 exports.getLecture = async (call, callback) => {
     const { id } = call.request;
 
-    const lectureRepository = getRepository(Lecture);
-    const downloadLecture = await lectureRepository.findOne(id);
+    let lectureRepository;
+    try {
+        lectureRepository = getRepository(Lecture);
+    } catch (error) {
+        const status = grpc.status.INTERNAL;
+        return errorHandler(callback, status, 'Database error1', error);
+    }
 
-    const lecture = {
-        id: downloadLecture.id,
-        name: downloadLecture.name,
-    };
+    let downloadLecture;
+    try {
+        downloadLecture = await lectureRepository.findOne(id);
+    } catch (error) {
+        const status = grpc.status.INTERNAL;
+        return errorHandler(callback, status, 'Database error2', error);
+    }
+
+    let lecture;
+    try {
+        lecture = {
+            id: downloadLecture.id,
+            name: downloadLecture.name,
+        };
+    } catch (error) {
+        const status = grpc.status.INTERNAL;
+        return errorHandler(callback, status, 'Database error3', error);
+    }
 
     callback(null, {lecture});
 };
 
 exports.getAllLectures = async (call, callback) => {
-    const lectures = await getRepository(Lecture)
-        .createQueryBuilder('lecture')
-        .where('lecture.course_id = :courseId',
-            { courseId: call.request.courseId })
-        .getMany();
-
+    let lectures;
+    try {
+        lectures = await getRepository(Lecture)
+            .createQueryBuilder('lecture')
+            .where('lecture.course_id = :courseId',
+                { courseId: call.request.courseId })
+            .getMany();
+    } catch (error) {
+        const status = grpc.status.INTERNAL;
+        return errorHandler(callback, status, 'Database error', error);
+    }
+    if (!lectures || !lectures.length) {
+        const status = grpc.status.INTERNAL;
+        return errorHandler(callback, status, 'Id not found');
+    }
     callback(null, {lectures});
 };
