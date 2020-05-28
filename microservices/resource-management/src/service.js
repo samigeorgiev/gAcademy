7aa09fed0441e702548ae40d2b5972b5b6e124e6
Samuil Georgiev <sami.georgiev@gmail.com>
2020-05-28 20:44:42 +0300

    [Update] Added jwt in rm

--- microservices/resource-management/src/service.js
+++ microservices/resource-management/src/service.js
@@ -1,90 +1,76 @@
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 const { getRepository } = require('typeorm');
 
-const ControlClient = require('./controlClient');
 const Resource = require('./models/resource');
 
 const router = new Router();
 
-const controlClient = new ControlClient(
-    process.env.RESOURCE_MANAGEMENT_CONTROL_URL
-);
-
 router.post('/upload/:token', (req, res) => {
-    controlClient.getResourceId(
-        { token: req.params.token },
-        (error, response) => {
-            if (error) {
-                // check for interbal error
-                return res.status(401).json({ message: 'Invalid token' });
-            }
-            const resourceId = response.id;
-            if (!req.files.resource) {
-                return res.status(422).json({ message: 'Missing resource' });
-            }
-            if (req.files.resource.mimetype !== 'video/mp4') {
-                return res.status(422).json({ message: 'Invalid filetype ' });
-            }
-            const filename = `v${new Date().getTime()}.mp4`;
-            req.files.resource.mv('resources/' + filename, async err => {
-                if (err) {
-                    return res.status(500).json({ message: 'FS error' });
-                }
+    let resourceId;
+    try {
+        resourceId = jwt.verify(req.params.token, process.env.JWT_SECRET)
+            .resourceId;
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
 
-                const resourceRepository = getRepository(Resource);
+    if (!req.files.resource) {
+        return res.status(422).json({ message: 'Missing resource' });
+    }
+    if (req.files.resource.mimetype !== 'video/mp4') {
+        return res.status(422).json({ message: 'Invalid filetype ' });
+    }
 
-                let resource;
-                try {
-                    resource = await resourceRepository.findOneOrFail(
-                        resourceId
-                    );
-                } catch (error) {
-                    // check for interbal error
-                    return res
-                        .status(422)
-                        .json({ message: 'Resource not found' });
-                }
-
-                resource.path = filename;
-                try {
-                    await resourceRepository.save(resource);
-                } catch (error) {
-                    return res.status(500).json({ message: 'Database error' });
-                }
-
-                res.status(200).json({ message: 'Success' });
-            });
+    const filename = `v${new Date().getTime()}.mp4`;
+    req.files.resource.mv('resources/' + filename, async error => {
+        if (error) {
+            return res.status(500).json({ message: 'FS error' });
         }
-    );
+
+        const resourceRepository = getRepository(Resource);
+
+        let resource;
+        try {
+            resource = await resourceRepository.findOneOrFail(resourceId);
+        } catch (error) {
+            // check for interbal error
+            return res.status(422).json({ message: 'Resource not found' });
+        }
+
+        resource.path = filename;
+        try {
+            await resourceRepository.save(resource);
+        } catch (error) {
+            return res.status(500).json({ message: 'Database error' });
+        }
+
+        res.status(200).json({ message: 'Success' });
+    });
 });
 
-router.get('/download/:token', (req, res) => {
-    controlClient.getResourceId(
-        { token: req.params.token },
-        async (error, response) => {
-            if (error) {
-                // check for interbal error
-                return res.status(401).json({ message: 'Invalid token' });
-            }
+router.get('/download/:token', async (req, res) => {
+    let resourceId;
+    try {
+        resourceId = jwt.verify(req.params.token, process.env.JWT_SECRET)
+            .resourceId;
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
 
-            const resourceId = response.id;
-            const resourceRepository = getRepository(Resource);
-            let resource;
-            try {
-                resource = await resourceRepository.findOneOrFail(resourceId);
-            } catch (error) {
-                // check from internal
-                return res.status(422).json({ message: 'Resource not found' });
-            }
+    const resourceRepository = getRepository(Resource);
+    let resource;
+    try {
+        resource = await resourceRepository.findOneOrFail(resourceId);
+    } catch (error) {
+        // check from internal
+        return res.status(422).json({ message: 'Resource not found' });
+    }
 
-            if (!resource.path) {
-                return res
-                    .status(503)
-                    .json({ message: 'Resource unavailable' });
-            }
-            res.download('resources/' + resource.path);
-        }
-    );
+    if (!resource.path) {
+        return res.status(503).json({ message: 'Resource unavailable' });
+    }
+    res.download('resources/' + resource.path);
 });
 
 module.exports = router;
