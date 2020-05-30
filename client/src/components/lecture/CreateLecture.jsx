21448540babb4a77052c0cba95679e4093faff66
Samuil Georgiev <sami.georgiev@gmail.com>
2020-05-30 09:50:13 +0300

    [Update] Added fetching in create lectur

--- client/src/components/lecture/CreateLecture.jsx
+++ client/src/components/lecture/CreateLecture.jsx
@@ -1,36 +1,65 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { Modal } from 'semantic-ui-react';
 
 import { AuthenticationContext } from '../../context/authentication';
 
 import useResourceManagementControl from '../../hooks/resourceManagementControl';
+import useUpload from '../../hooks/upload';
 
 import { CreateLectureRequest } from '../../proto/resource-management-control_pb';
 
 import Form from '../UI/Form';
 
 const CreateLecture = props => {
+    const [resource, setResource] = useState(null);
+
     const { user } = useContext(AuthenticationContext);
 
-    const { state, methods } = useResourceManagementControl();
+    const {
+        state: resourceManagementControlState,
+        methods: resourceManagementControlMethods
+    } = useResourceManagementControl();
+    const { state: uploadState, sendRequest: uploadSendRequest } = useUpload();
 
     const { course } = props;
-    const { createLecture } = methods;
+    const { createLecture } = resourceManagementControlMethods;
     const { token } = user;
     const submitHandler = inputsValues => {
         const request = new CreateLectureRequest();
         request.setCourseid(course);
         request.setName(inputsValues.name);
         createLecture(request, token);
+        setResource(inputsValues.resource);
     };
 
-    const { response, error } = state;
+    const {
+        response: resourceManagementControlResponse,
+        error: resourceManagementControlError
+    } = resourceManagementControlState;
     useEffect(() => {
-        if (response && !error) {
-            console.log(response);
+        if (
+            resourceManagementControlResponse &&
+            !resourceManagementControlError &&
+            resource
+        ) {
+            const url = resourceManagementControlResponse.getUrl();
+            uploadSendRequest(url, resource);
         }
-    }, [response, error]);
+    }, [
+        resourceManagementControlResponse,
+        resourceManagementControlError,
+        resource,
+        uploadSendRequest
+    ]);
+
+    const { response: uploadResponse, error: uploadError } = uploadState;
+    const { onClose } = props;
+    useEffect(() => {
+        if (uploadResponse && !uploadError) {
+            onClose(true);
+        }
+    });
 
     const inputs = {
         name: {
@@ -51,6 +80,10 @@ const CreateLecture = props => {
         }
     };
 
+    const isLoading =
+        resourceManagementControlState.isLoading || uploadState.isLoading;
+    const error = resourceManagementControlState.error || uploadState.error;
+
     return (
         <Modal
             onClose={props.onClose}
@@ -64,8 +97,8 @@ const CreateLecture = props => {
                 <Form
                     inputs={inputs}
                     onSubmit={submitHandler}
-                    isLoading={state.isLoading}
-                    error={state.error}
+                    isLoading={isLoading}
+                    error={error && error.message}
                 />
             </Modal.Content>
         </Modal>
